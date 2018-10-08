4ba1f4dd7e6a7a8c1fd3fa055efe4b17cc422a22
lilgio <38165365+lilgio@users.noreply.github.com>
2018-10-09 01:53:17 +0200

    Create typeracer.js

---
+++ typeracer.js
@@ -0,0 +1,27 @@
+// Import Jquery
+var script = document.createElement('script');
+var src = "https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js";
+script.src = src;
+document.body.append(script);
+
+function sleep (time) {
+  return new Promise((resolve) => setTimeout(resolve, time));
+}
+
+await sleep(1000); // Wacht een seconde totdat Jquery is ingeladen
+
+var text = "";
+var inputField = $('.txtInput');
+
+async function addText(character){
+	$(inputField).val($(inputField).val() + character);
+}
+
+$('span[unselectable="on"]').each(function(a,b){
+	text += b.innerHTML;
+});
+
+for(var i = 0; i<= text.length; i++){
+	addText(text[i]);
+	await sleep(105);
+}
